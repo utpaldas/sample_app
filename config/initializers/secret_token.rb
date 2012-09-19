dd2abbfece24b02912684a1798e64a4d2b81da54
utpaldas <utpal.das@gmail.com>
2012-09-19 10:42:12 -0700

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '400a0628119322eb96156091c33976d3eb07b89ff4b3bc76638b1dda4d601c03754f904b0aecd4c00eceb7ec78055ddfefbbf9d3fd193f13b304f53436176cae'
