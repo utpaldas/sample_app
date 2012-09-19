51177a0143b0f2fbf79591564a3812b29e9a7807
utpaldas <utpal.das@gmail.com>
2012-09-19 14:49:45 -0700

    Added Title Test

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,25 +1,36 @@
 require 'spec_helper'
 
 describe "StaticPages" do
-  describe "Home Page" do
-    it "should have the content 'Sample Ruby Application'" do
+  describe "GET home" do
+    it "should have the content 'Sample App'" do
       visit '/static_pages/home'
-      page.should have_content("Sample Ruby Application")
-      page.should have_css("title", text: "Sample Ruby Application")
+      page.should have_selector('h1', text: "Sample App")
+    end
+    it "should have the right title" do
+      visit '/static_pages/home'
+      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Home")
     end
   end
 
-  describe "Help Page" do
+  describe "GET help" do
     it "should have the content 'Help'" do
       visit '/static_pages/help'
       page.should have_content("Help")
     end
+    it "should have the right title" do
+      visit '/static_pages/help'
+      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | Help")
+    end
   end
 
-  describe "About Page" do
+  describe "GET about" do
     it "should have the content 'About Us'" do
       visit '/static_pages/about'
       page.should have_content('About Us')
+    end
+    it "should have the right title" do
+      visit '/static_pages/about'
+      page.should have_selector('title', text: "Ruby on Rails Tutorial Sample App | About Us")
     end
   end
 end
