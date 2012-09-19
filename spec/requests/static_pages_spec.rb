573037cecbb06bf451a2649b07490308a62452d8
utpaldas <utpal.das@gmail.com>
2012-09-19 14:15:48 -0700

    Added About Us Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -10,9 +10,16 @@ describe "StaticPages" do
   end
 
   describe "Help Page" do
-    it "should help the content 'Help'" do
+    it "should have the content 'Help'" do
       visit '/static_pages/help'
       page.should have_content("Help")
+    end
+  end
+
+  describe "About Page" do
+    it "should have the content 'About Us'" do
+      visit '/static_pages/about'
+      page.should have_content('About Us')
     end
   end
 end
