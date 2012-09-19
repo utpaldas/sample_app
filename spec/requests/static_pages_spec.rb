fed09ca2d99c7be7eae9b36cb46b2f5b121f5a27
utpaldas <utpal.das@gmail.com>
2012-09-19 14:06:20 -0700

    Added Help Page

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -2,10 +2,17 @@ require 'spec_helper'
 
 describe "StaticPages" do
   describe "Home Page" do
-    it "works! (now write some real specs)" do
+    it "should have the content 'Sample Ruby Application'" do
       visit '/static_pages/home'
       page.should have_content("Sample Ruby Application")
       page.should have_css("title", text: "Sample Ruby Application")
+    end
+  end
+
+  describe "Help Page" do
+    it "should help the content 'Help'" do
+      visit '/static_pages/help'
+      page.should have_content("Help")
     end
   end
 end
